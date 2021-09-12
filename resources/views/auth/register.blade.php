693c31b0ee2dc7909a7964c09471f6552c528c1b
voidf.0xb3 <the.invisible.phantom@gmail.com>
2021-09-12 19:28:46 +0300

    Add Fortify stub

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>Регистрация</title>
+    </head>
+    <body>
+        <h1>Регистрация</h1>
+    </body>
+</html>
