045b1539cf4e133083852cf8021964b6c26ef5e4
julialunati <julia.molokova11@icloud.com>
2021-09-15 13:55:18 +0200

    added view my races

--- resources/views/user/account.blade.php
+++ resources/views/user/account.blade.php
@@ -66,38 +66,22 @@
             </div>
             <div class="col-lg-8 pb-5">
 
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div>
+                    <h3>МОИ ЗАБЕГИ</h3>
+                </div> <br><br>
+                @forelse($races as $race)
+                <div>
+                    <div>{{ $race->title }}</div>
+                    <div>{{ $race->date }}</div>
+                    <div>{{ $race->distance }} метров</div>
+                    <div><a href="#">отменить регистрацию</a></div>
+                </div><br>
+                @empty
+                <br><br><br>
+                <div>У вас пока что нет ни одной регистрации на забег!</div>
+                <div><a href="{{ route('index')}}">Записаться на мой первый забег</a></div>
+                @endforelse
             </div>
-
         </div>
     </div>
 
