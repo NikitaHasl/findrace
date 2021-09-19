601398a5780fd76526bc6fe79fe0cb740cfd696a
David <braginsky.david@yandex.ru>
2021-09-19 14:14:45 +0300

    added race results to account

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// RACE //
 Route::resource('races', RaceController::class)
     ->except(['edit', 'destroy', 'update']);
 
+Route::get('/', [RaceController::class, 'index'])
+    ->name('index');
 Route::get('/', [RaceController::class, 'index'])->name('index');
 
 Route::get('/races/{race}/participants',
     [RaceController::class, 'listParticipants'])->name('listParticipants');
 
+// AUTH //
 Route::get('/logout', function () {
     return view('auth.logout');
 })->name('logout');
@@ -36,11 +40,25 @@ Route::get('/register', function() {
     return view('auth.register');
 })->name('register');
 
-Route::get('/account', [AccountController::class, 'index'])->name('account');
+// ACCOUNT //
+Route::get('/account', [AccountController::class, 'index'])
+    ->name('account');
 
-Route::get('/subscribe/{id}', [AccountController::class, 'subscribe'])->where('id', '\d+')->name('subscribe');
-Route::get('/account/unsubscribe/{race_id}', [AccountController::class, 'unsubscribe'])->where('id', '\d+')->name('unsubscribe');
+Route::get('/account/active', [AccountController::class, 'showActiveRaces'])
+    ->name('account.active');
 
+Route::get('/account/finished', [AccountController::class, 'showFinishedRaces'])
+    ->name('account.finished');
+
+Route::get('/subscribe/{id}', [AccountController::class, 'subscribe'])
+    ->where('id', '\d+')
+    ->name('subscribe');
+
+Route::get('/account/unsubscribe/{race_id}', [AccountController::class, 'unsubscribe'])
+    ->where('id', '\d+')
+    ->name('unsubscribe');
+
+// SEARCH //
 Route::get('/search', [RaceController::class, 'search'])
     ->name('search');
 
