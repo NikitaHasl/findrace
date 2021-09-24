91076d014eed645d263f2fce7b157eeba4a1c965
julialunati <julia.molokova11@icloud.com>
2021-09-24 20:27:47 +0200

    view registration/login left-side

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,26 +1,48 @@
-@extends('layout.main')
+@extends('layout.reglog')
 
 @section('title', 'Регистрация')
 
 @section('content')
-    <div class="registration">
-        <h1 class="registration__header">Регистрация</h1>
-        @foreach($errors->all() as $error)
-            <p class="error">{{ $error }}</p>
-        @endforeach
-        <form action="/register" method="post">
-            @csrf
-            <label class="registration__field">Имя: <input name="firstname"></label>
-            <label class="registration__field">Фамилия: <input name="lastname"></label>
-            <label class="registration__field">Почта: <input type="email" name="email"></label>
-            <label class="registration__field">Пароль: <input type="password" name="password"></label>
-            <label class="registration__field">Повтор пароля: <input type="password" name="password_confirmation"></label>
+
+<div class=registration-header>
+    <div class="registration-title">Регестрируйся и беги</div>
+    <div class="registration-text">Присоединяйся к нам и бегай по всей России!</div>
+</div>
+
+
+@foreach($errors->all() as $error)
+<p class="error">{{ $error }}</p>
+@endforeach
+
+<div class=registration-form>
+    <form action="/register" method="post">
+        @csrf
+
+        <div class="form-group">
+            <label class="registration-field">Имя <input name="firstname" placeholder="Введи свое имя"></label>
+        </div>
+
+        <!-- <label class="registration__field">Фамилия: <input name="lastname"></label> -->
+        <div class="form-group">
+            <label class="registration-field">E-mail: <input type="email" name="email" placeholder="Введи свою почту"></label>
+        </div>
+        <div class="form-group">
+            <label class="registration-field">Пароль: <input type="password" name="password" placeholder="Задай свой пароль"></label>
+        </div>
+        <div class="form-group">
+            <label class="registration-field"><input type="password" name="password_confirmation" value="Повтори пароль"></label>
+        </div>
+        <div class="form-group">
             <fieldset>
                 <legend>Роль</legend>
-                <label class="registration__field"><input type="radio" name="role" value="user" checked> Участник</label>
-                <label class="registration__field"><input type="radio" name="role" value="organizer"> Организатор</label>
+                <label class="registration-field"><input type="radio" name="role" value="user" checked> Участник</label>
+                <label class="registration-field"><input type="radio" name="role" value="organizer"> Организатор</label>
             </fieldset>
-            <input class="registration__btn" type="submit" value="Зарегистрироваться">
-        </form>
-    </div>
+        </div>
+        <div class="form-group">
+            <input class="registration-btn" type="submit" value="Зарегистрироваться">
+        </div>
+    </form>
+</div>
+
 @endsection
