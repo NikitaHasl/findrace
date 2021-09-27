de212527dcf8869922e2b1b46a7fa00fe45fe61a
davidbraginsky <64259294+davidbraginsky@users.noreply.github.com>
2021-09-27 17:01:53 +0300

    Update validation.php

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -149,7 +149,7 @@ return [
     |
     | The following language lines are used to swap our attribute placeholder
     | with something more reader friendly such as "E-Mail Address" instead
-    | of "email". This simply helps us make our chat more expressive.
+    | of "email". This simply helps us make our message more expressive.
     |
     */
 
