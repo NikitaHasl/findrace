26e391423c7872a166c36904075959cb0e3d0a7d
Nikita <khodosov97@mail.ru>
2021-09-28 11:25:58 +0300

    fix migrations

--- database/migrations/2021_09_28_080540_change_column_city_on_cities_table.php
+++ database/migrations/2021_09_28_080540_change_column_city_on_cities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCitiesTable extends Migration
+class ChangeColumnCityOnCitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreateCitiesTable extends Migration
      */
     public function up()
     {
-        Schema::create('cities', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('country_id')
-                ->constrained('countries')
-                ->onUpdate('cascade');
-            $table->string('city', 45);
-            $table->timestamps();
+        Schema::table('cities', function (Blueprint $table){
+            $table->renameColumn('city', 'city_title');
         });
     }
 
@@ -30,6 +25,8 @@ class CreateCitiesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cities');
+        Schema::table('cities', function (Blueprint $table){
+            $table->renameColumn('city_title', 'city');
+        });
     }
 }
