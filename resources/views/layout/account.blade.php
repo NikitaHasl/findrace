95cfa0ef573272dc9ab0c93fd3e1713ff44f6bc0
julialunati <julia.molokova11@icloud.com>
2021-09-28 18:35:52 +0200

    left-side account main

---
+++ resources/views/layout/account.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/css/styleAccount.css') }}">
+    <script defer src="{{ asset('assets/js/app.js') }}"></script>
+    <title>@yield('title')</title>
+    @yield('head-extra')
+</head>
+
+<body>
+    <div class="wrapper">
+
+        <header class="header">
+            <a class="logo-text" href="{{ route('index') }}">FindRace</a>
+            <a class="account-text" href="{{ route('index') }}">Главная</a>
+            <a class="account-text" href="#">Чат</a>
+            <a class="account-text" href="{{ route('account') }}">Профиль</a>
+        </header>
+
+        <main class="main">
+            @yield('content')
+        </main>
+
+        <footer class="footer">
+            <div class="footer-left">
+                <a class="account-text" href="{{ route('index') }}">Все забеги</a>
+            </div>
+            <div class="footer-right">
+                <a class="account-text" href="#">Правила использования</a>
+                <a class="account-text" href="#">Политика конфиденциальности</a>
+                <span class="account-text">FindRace&copy;2021</span>
+            </div>
+        </footer>
+
+    </div>
+</body>
+
+</html>
