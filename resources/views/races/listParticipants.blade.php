6422588c51b7d908f38ba122b1c48c5567807079
voidf.0xb3 <the.invisible.phantom@gmail.com>
2021-09-30 16:33:12 +0300

    Remove `@csrf` from a form on the list participants page

--- resources/views/races/listParticipants.blade.php
+++ resources/views/races/listParticipants.blade.php
@@ -13,44 +13,43 @@
 </div>
 
 @if(count($participants) >= 1)
-<table>
-    <tr class="table-top">
-        <th>Фото</th>
-        <th>Имя</th>
-        <th>Фамилия</th>
-        <th>Время</th>
-        <th>Место</th>
-    </tr>
-    @foreach($participants as $participant)
-    <tr class="table-row">
-        <th>@if($participant->avatar)
-            <img class="img-org-displ" src="{{ asset('storage/' . $participant->avatar) }}">
-            @endif
-        </th>
-        <th>{{ $participant->firstname }}</th>
-        <th>{{ $participant->lastname }}</th>
-        <th>@if($participant->finish_time)
-            {{ $participant->finish_time }}
-            @else
-            нет инфо
-            @endif
-        </th>
-        <th>@if($participant->place)
-            {{ $participant->place }}
-            @else
-            нет инфо
-            @endif
-        </th>
-    </tr>
-    @endforeach
-</table>
+    <table>
+        <tr class="table-top">
+            <th>Фото</th>
+            <th>Имя</th>
+            <th>Фамилия</th>
+            <th>Время</th>
+            <th>Место</th>
+        </tr>
+        @foreach($participants as $participant)
+        <tr class="table-row">
+            <th>@if($participant->avatar)
+                <img class="img-org-displ" src="{{ asset('storage/' . $participant->avatar) }}">
+                @endif
+            </th>
+            <th>{{ $participant->firstname }}</th>
+            <th>{{ $participant->lastname }}</th>
+            <th>@if($participant->finish_time)
+                {{ $participant->finish_time }}
+                @else
+                нет инфо
+                @endif
+            </th>
+            <th>@if($participant->place)
+                {{ $participant->place }}
+                @else
+                нет инфо
+                @endif
+            </th>
+        </tr>
+        @endforeach
+    </table>
 @endif
 @empty(count($participants))
-<div class="no-run">Еще никто не записался на данный забег.</div>
+    <div class="no-run">Еще никто не записался на данный забег.</div>
 @endempty
 
 <form class="back-form back-left" action="{{ route('account.races') }}" method="get">
-    @csrf
     <input class="update-btn" type="submit" value="Обратно">
 </form>
 
